2569c81b0e840f65baa6d1ffdce0cdd83ae6125d
Sherman Chen <uxdailygeek@gmail.com>
2020-02-06 04:24:21 +0800

    Added additional code samples to Declaration_Assignments

---
+++ W3Schools/Declaration_Assignments/UsingExtern/cmake-build-debug/CMakeFiles/UsingExtern.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UsingExtern.dir/main.c.obj"
+  "UsingExtern.exe"
+  "UsingExtern.exe.manifest"
+  "UsingExtern.lib"
+  "UsingExtern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UsingExtern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
