a1de0fcf4aef146d2593cf66b72fe951cf4a052d
Sherman Chen <uxdailygeek@gmail.com>
2020-03-04 22:30:02 +0800

    Added another code example

--- Data Structures Using C/Arrays/SmallestNum/main.c
+++ Data Structures Using C/Arrays/SmallestNum/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
-	printf ( "Hello, World!\n" );
+	
+	int i, n, arr[20], small, pos;
+	system ("cls");
+	printf("\n Enter the number of elements in the array: ");
+	scanf("%d", &n);
+	printf("\n Enter the elements : ");
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+	small = arr[0];
+	pos = 0;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < small)
+		{
+			small = arr[i];
+			pos = i;
+		}
+	}
+	
+	printf("\n The smallest element is: %d", small);
+	printf("\n The position of the smallest element in the array is: %d", pos);
+	
 	return 0;
 }
